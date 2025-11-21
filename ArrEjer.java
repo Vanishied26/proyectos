0883629af790fde95d143671602ab017c044c124
Vanishied26 <nicolasgonzalezorozco2006@gmail.com>
2025-11-21 13:40:57 -0600

    Create ArrEjer.java

---
+++ ArrEjer.java
@@ -0,0 +1,23 @@
+package arrejer1;
+import java.util.*;
+public class ArrEjer1{
+    public static void main(String[] args){
+        Scanner tecla=new Scanner(System.in);
+        int num1;
+        int[] num= new int[5];
+        num[0]=1;
+        num[1]=2;
+        num[2]=3;
+        num[3]=4;
+        num[4]=5;
+        System.out.println("Que numero desea buscar");
+        num1=tecla.nextInt();
+        for(int i=0;i<num.length;i++){
+            if(num.length==num1)   
+               System.out.println("El numero esta en el arreglo");
+            else 
+                System.out.println("El numero no  esta en el arreglo");
+        }
+    }
+    
+}
